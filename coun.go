37c2ea43d7198dfc40af1a8d441bd99bb256d432
hanapiko <168718270+hanapiko@users.noreply.github.com>
2024-05-20 18:57:35 +0300

    Add files via upload

---
+++ coun.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"os"
+
+	"github.com/01-edu/z01"
+)
+
+func main() {
+	args := os.Args[1:]
+	argum := len(args)
+	count := 0
+	for _, ch := range argum {
+		count++
+		z
+	}
+}
