718889ef0aed9d18d89437f80c104ff62181d271
hanapiko <168718270+hanapiko@users.noreply.github.com>
2024-06-04 18:16:11 +0300

    Add files via upload

---
+++ reduceint/main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"fmt"
+
+	// "github.com/01-edu/z01"
+)
+
+func main() {
+	mul := func(acc int, cur int) int {
+		return acc * cur
+	}
+	sum := func(acc int, cur int) int {
+		return acc + cur
+	}
+	div := func(acc int, cur int) int {
+		return acc / cur
+	}
+	as := []int{50000,2}
+	ReduceInt(as, mul)
+	ReduceInt(as, sum)
+	ReduceInt(as, div)
+}
+func ReduceInt(a []int, f func(int, int)int){
+	result := a[0]
+	for i := 1; i < len(a); i++ {
+		result = f(result, a[i])
+	}
+	fmt.Println(result)
+}
+
+
+// func ReduceInt(a []int, f func(int, int) int){
+// 	result := a[0]
+// 	for i := 1; i < len(a); i++ {
+// 		result = f(result, a[i])
+// 	}
+// 	Num(result)
+// 	z01.PrintRune('\n')
+// }
+// func Num(n int) {
+// 	if n < 0 {
+// 		return 
+// 	}
+// 	slice := make([]int, 0)
+// 	for n > 0 {
+// 		slice = append(slice, n%10)
+// 		n /= 10
+// 	}
+// 	for i := len(slice)-1; i >= 0; i--{
+// 		z01.PrintRune(rune(slice[i] + '0'))
+// 	}
+
+// }
