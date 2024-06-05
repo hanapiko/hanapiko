fbecee870fd12d037e9b47e02eb32dc8f3b95bc4
hanapiko <168718270+hanapiko@users.noreply.github.com>
2024-06-05 18:07:06 +0300

    Add files via upload

---
+++ compare/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	
+)
+
+func main() {
+	fmt.Println(Compare("Hello!", "Hello!"))
+	fmt.Println(Compare("Salut!", "lut!"))
+	fmt.Println(Compare("Ola!", "Ol"))
+}
+
+func Compare(a, b string) int {
+	if a == b {
+		return 0
+	} else if a > b {
+		return 1
+	}
+	return -1
+}
