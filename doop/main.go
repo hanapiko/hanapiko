55bfd9dfdf855e7db3858f8a9feda5de469aea91
hanapiko <168718270+hanapiko@users.noreply.github.com>
2024-07-11 17:42:25 +0300

    Add files via upload

--- doop/main.go
+++ doop/main.go
@@ -1,14 +1,13 @@
 package main
 
 import (
-	"fmt"
 	"os"
 )
 
 func main() {
 	args := os.Args[1:]
 	if len(args) != 3 {
-		//fmt.Println("usage : go run . int op int")
+		// fmt.Println("usage : go run . int op int")
 		return
 	}
 	int1 := args[0]
@@ -16,23 +15,75 @@ func main() {
 	int2 := args[2]
 	result := 0
 
-	if operator == "+" {
-		result = Atoi(int1) + Atoi(int2)
-	} else if operator == "-" {
-		result = Atoi(int1) - Atoi(int2)
-	} else if operator == "*" {
-		result = Atoi(int1) * Atoi(int2)
-	} else {
+	num1 := Atoi(int1)
+	num2 := Atoi(int2)
+
+	if num1 >= 9223372036854775805 || num1 <= -9223372036854775805 {
+		os.Exit(0)
 		return
 	}
-	fmt.Println(result)
+
+	if num2 >= 9223372036854775805 || num1 <= -9223372036854775805 {
+		os.Exit(0)
+		return
+	}
+
+	if operator != "*" && operator != "/" && operator != "+" && operator != "-" && operator != "%" {
+		os.Exit(0)
+		return
+	}
+
+	switch operator {
+	case "+":
+		result = num1 + num2 
+	case  "-":
+		result = num1 - num2
+	case "*":
+		result = num1 * num2
+	case "/":
+		if num2 == 0 {
+			os.Stdout.WriteString("No division by 0")
+			 return
+		}
+		result = num1 / num2
+	case "%":
+		if num2 == 0 {
+			os.Stdout.WriteString("No modulo by 0")
+			 return
+		}
+		result = num1 % num2
+	}
+	
+    os.Stdout.WriteString(itoa(result)+ "\n")
+	//os.Stdout.WriteString("\n")
+
 }
+
+func itoa(n int) string {
+	sign := ""
+	res := ""
+
+	if n == 0 {
+		return "0"
+	}
+	if n < 0 {
+		sign = "-"
+		n = -n
+	}
+
+	for n > 0 {
+		res = string(n%10+'0') + res
+		n /= 10
+	}
+	return sign + res
+}
+
 
 func Atoi(s string) int {
 	multiplier := 1
 	val := 0
 	for i, ch := range s {
-		if i == 0 && ch == '-'{
+		if i == 0 && ch == '-' {
 			multiplier = -1
 			continue
 		} else if i == 0 && ch == '+' {
@@ -40,9 +91,10 @@ func Atoi(s string) int {
 			continue
 		}
 		if ch < '0' || ch > '9' {
-			os.Exit(1)
+			os.Exit(0)
 		}
 		val = val*10 + (int(ch - '0'))
 	}
+
 	return val * multiplier
 }
